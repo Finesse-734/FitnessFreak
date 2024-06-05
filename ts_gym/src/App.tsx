de85516fd8994202504564464ab2a0a7f42040a0
Finesse-734 <gupta.nikunj734@gmail.com>
2024-06-06 00:44:11 +0530

    Completed the frontend for the website with the landing page fully responsive

--- ts_gym/src/App.tsx
+++ ts_gym/src/App.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourclasses";
+import ContactUs from "@/scenes/contactus";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -32,6 +34,8 @@ function App() {
       <Home setSelectedPage={selectedPage}/>
       <Benefits setSelectedPage = {selectedPage}/>
       <OurClasses setSelectedPage={selectedPage}/>
+      <ContactUs setSelectedPage={selectedPage}/>
+      <Footer/>
     </div>
   )
 }
